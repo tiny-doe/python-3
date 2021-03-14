87d87fbf79e3e93ebd4e5f1c16c04cac557833b9
Zoe Steinbean <80606138+tiny-doe@users.noreply.github.com>
2021-03-13 18:33:45 -0600

    Create block-letters.py

---
+++ block-letters.py
@@ -0,0 +1,10 @@
+#It's me Zoe Steinbean (i'm watching u FBI) and i haven't cried yet today!
+
+#initials time ZRS
+print("ZZZZZ" + "  " + "RRRR " + "  " + " SSS ")
+print("    Z" + "  " + "R   R" + "  " + "S   S")
+print("   Z " + "  " + "R   R" + "  " + "S    ")
+print("  Z  " + "  " + "RRRR " + "  " + " SSS ")
+print(" Z   " + "  " + "R R  " + "  " + "    S")
+print("Z    " + "  " + "R  R " + "  " + "S   S")
+print("ZZZZZ" + "  " + "R   R" + "  " + " SSS")
